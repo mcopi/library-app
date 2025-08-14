7d002028a19045254a4cd11cec255962cf9c90bf
mcopi <95206824+mcopi@users.noreply.github.com>
2025-08-15 00:03:19 +0700

    enhancement/gateway security

--- api-gateway/src/main/java/com/cop/api_gateway/config/WebSecurityConfig.java
+++ api-gateway/src/main/java/com/cop/api_gateway/config/WebSecurityConfig.java
@@ -3,20 +3,21 @@ package com.cop.api_gateway.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(auth -> {
-                    auth.pathMatchers(HttpMethod.POST, "/user/login").permitAll()
-                            .pathMatchers(HttpMethod.POST, "/user/register").permitAll()
-                            .anyExchange().authenticated();
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(HttpMethod.POST, "/user/login").permitAll()
+                            .requestMatchers(HttpMethod.POST, "/user/register").permitAll()
+                            .anyRequest().authenticated();
                 })
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();

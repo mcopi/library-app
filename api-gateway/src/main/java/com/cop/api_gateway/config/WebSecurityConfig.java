e183b0c3a36d34b07300fea9f95cc87236c315a7
mcopi <95206824+mcopi@users.noreply.github.com>
2025-08-14 23:07:52 +0700

    enhancement/gateway with auth server

---
+++ api-gateway/src/main/java/com/cop/api_gateway/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.cop.api_gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(auth -> {
+                    auth.pathMatchers(HttpMethod.POST, "/user/login").permitAll()
+                            .pathMatchers(HttpMethod.POST, "/user/register").permitAll()
+                            .anyExchange().authenticated();
+                })
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
